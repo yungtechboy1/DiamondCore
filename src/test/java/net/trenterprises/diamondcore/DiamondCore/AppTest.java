98c9e0f3094e31dec6759bcf60b0f832e97d4e4f
DRAGKILLS <70339357+DRAGKILLS@users.noreply.github.com>
2021-06-13 21:30:48 +0300

    Update AppTest.java

--- src/test/java/net/trenterprises/diamondcore/DiamondCore/AppTest.java
+++ src/test/java/net/trenterprises/diamondcore/DiamondCore/AppTest.java
@@ -4,20 +4,16 @@ import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
+public class AppTest extends TestCase
 {
     /**
      * Create the test case
      *
      * @param testName name of the test case
      */
-    public AppTest( String testName )
+    public AppTest(String testName)
     {
-        super( testName );
+        super(testName);
     }
 
     /**
@@ -25,7 +21,7 @@ public class AppTest
      */
     public static Test suite()
     {
-        return new TestSuite( AppTest.class );
+        return new TestSuite(AppTest.class);
     }
 
     /**
@@ -33,6 +29,6 @@ public class AppTest
      */
     public void testApp()
     {
-        assertTrue( true );
+        assertTrue(true);
     }
 }
